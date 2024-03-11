8e339031a8bf4d377a3baf186ca7600bbb267134
semperprimum <bkeem0266+github@gmail.com>
2024-03-11 16:27:41 +0500

    add a simple media query for the desktop

--- src/components/AppContentWrapper.styled.ts
+++ src/components/AppContentWrapper.styled.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const AppContentWrapper = styled.div`
   width: calc(100% - 2rem);
   margin-inline: auto;
+
+  @media only screen and (min-width: 90em) {
+    width: min(60em, calc(100% - 2rem));
+  }
 `;
